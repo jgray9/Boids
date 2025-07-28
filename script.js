7414af13e599e51ef5c6cce652cd94793f02d4d2
jgray9 <jgray9@umbc.edu>
2025-07-28 16:40:06 -0400

    created function for drawing boids

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ class Boid {
         this.vy = 0;
     }
 }
+
+const canvas = document.getElementById("boidbox");
+const ctx = canvas.getContext("2d");
+const boids = [];
+
+function update_boids() {
+    ctx.clearRect(0, 0, 1200, 600);
+    for (let b in boids) {
+        // physics here
+
+        ctx.fillRect(b.x, b.y, 20, 20);
+    }
+}
