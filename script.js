7de51691d03746d38e2269580005a6e9add5797a
jgray9 <jgray9@umbc.edu>
2025-07-29 14:36:16 -0400

    added collision avoidance force

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ class Boid {
         this.vx = 0;
         this.vy = 0;
     }
+
+    distance(b) {
+        let dx = this.x - b.x;
+        let dy = this.y - b.y;
+        return Math.sqrt(dx*dx+dy*dy);
+    }
 }
 
 const boids = [
@@ -20,8 +26,18 @@ function update_boids() {
     ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, 1200, 600);
     for (let b of boids) {
-        // physics here
+        console.log(b.x + "," + b.y + " " + b.vx + "," + b.vy);
+        for (let n of boids) {
+            if(b == n) continue;
+            if(b.distance(n) > 10) continue;
+            b.vx += b.x - n.x;
+            b.vy += b.y - n.y;
+        }
+    }
 
+    for (let b of boids) {
+        b.x += b.vx;
+        b.y += b.vy;
         ctx.fillRect(b.x, b.y, 20, 20);
     }
 }
