153496178dc97fb16d79d62db27298b6bcdf2ccc
jgray9 <jgray9@umbc.edu>
2025-08-05 11:48:13 -0400

    implement insert functions

--- classes.js
+++ classes.js
@@ -9,11 +9,36 @@ class KDTree {
     root = null;
 
     insert(b) {
-
+        if (this.root == null) {
+            this.root = {
+                boid: b,
+                left: null,
+                right: null
+            };
+        } else
+            this.insertr(b, this.root, true);
     }
 
     insertr(b, node, is_x) {
+        let bc = is_x ? b.p.x : b.p.y;
+        let nc = is_x ? node.boid.p.x : node.boid.p.y;
+        let new_node = {
+            boid: b,
+            left: null,
+            right: null
+        };
 
+        if (bc < nc) {
+            if (node.left == null)
+                node.left = new_node;
+            else
+                this.insertr(b, node.left, !is_x);
+        } else {
+            if (node.right == null)
+                node.right = new_node;
+            else
+                this.insertr(b, node.right, !is_x);
+        }
     }
 
     search(b) {
