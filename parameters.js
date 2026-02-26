7f52bfe9e5c664b0ea6c2f7378b9b449ab0f1591
jgray <jgray9@umbc.edu>
2026-02-26 13:54:32 -0500

    reduced neighbor radius

--- parameters.js
+++ parameters.js
@@ -3,7 +3,7 @@ const BPS = 10;                 // number of boids spawned per second when mouse
 const BSIZE = 5;                // size of boids
 const LSIZE = 0.5;              // size of neighbor lines
 const COLLISION_RADIUS = 20;    // max distance between boids for collision avoidance
-const NEIGHBOR_RADIUS = 100;    // max distance between boids for velocity matching and centering
+const NEIGHBOR_RADIUS = 50;     // max distance between boids for velocity matching and centering
 
 const COLLISION_FORCE = 0.05;   // modifier for collision avoidance force
 const VELOCITY_FORCE = 0.05;    // modifier for velocity matching force
