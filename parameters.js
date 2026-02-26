15c8482a3fb56a75c7c2c5bab3329715ec9ac361
jgray <jgray9@umbc.edu>
2026-02-25 19:01:32 -0500

    adjusted boid size to new canvas size

--- parameters.js
+++ parameters.js
@@ -1,8 +1,8 @@
 const dt = 0.05;    // timestep in seconds
-const BSIZE = 2;                // size of boids
+const BSIZE = 5;                // size of boids
 const LSIZE = 0.5;              // size of neighbor lines
-const NEIGHBOR_RADIUS = 20;     // distance 
-const STEERING_RADIUS = 20;     
+const NEIGHBOR_RADIUS = 40;     // distance 
+const STEERING_RADIUS = 40;     
 
 const COLLISION_FORCE = 1;      // modifier for collision avoidance force
 const VELOCITY_FORCE = 1;       // modifier for velocity matching force
@@ -11,4 +11,4 @@ const BORDER_FORCE = 10;        // modifier for border avoidance force
 
 LVISIBLE = false;
 
-const MIN_SPEED = 20;   // minimum speed for any boid
+const MIN_SPEED = 40;   // minimum speed for any boid
