6619fcabef1b7ebf0967b6b9e039e2d92b2ab456
jgray <jgray9@umbc.edu>
2026-06-05 07:59:34 -0400

    added structure for boid update loop

--- src/Boidbox.jsx
+++ src/Boidbox.jsx
@@ -2,11 +2,19 @@ import { useEffect, useRef } from 'react'
 import './Boidbox.css'
 
 function Boidbox() {
-  let canvasRef = useRef(null);
+  function updateBoids(canvas, ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // boid logic
+  }
+
+  let canvasRef = useRef(null);
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
+    let canvas = canvasRef.current;
+    let ctx = canvas.getContext('2d');
+
+    let timerID = setInterval(() => updateBoids(canvas, ctx), 1000 / 60);
+    return () => clearTimeout(timerID);
   }, []);
 
   return <canvas id='boidbox' ref={canvasRef} width={1200} height={600} />
