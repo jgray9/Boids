0d12c82f90d55a59ec31872a2542d858e689ef7d
jgray <jgray9@umbc.edu>
2026-06-05 08:11:19 -0400

    draw boids

--- src/Boidbox.jsx
+++ src/Boidbox.jsx
@@ -2,10 +2,25 @@ import { useEffect, useRef } from 'react'
 import './Boidbox.css'
 
 function Boidbox() {
+  let boids = useRef([
+    {
+      'x': 10,
+      'y': 10
+    },
+    {
+      'x': 20,
+      'y': 20
+    }
+  ]);
+
   function updateBoids(canvas, ctx) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // boid logic
+    boids.current.forEach(b => {
+      ctx.rect(b.x - 10, b.y - 10, 20, 20);
+    });
+    ctx.stroke();
   }
 
   let canvasRef = useRef(null);
